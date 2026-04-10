e64a974127a272bd3f4b99d05b00fb285b71a165
sadatech <github@sada.id>
2026-04-10 15:21:08 +0000

    Update tile layer source to edge-tiles.sadata.id in leaflet_options.js

--- src/leaflet_options.js
+++ src/leaflet_options.js
@@ -7,8 +7,7 @@ let L = require('leaflet')
 
 // tile.openstreetmap.org
 // edge-tiles.sadata.id
-let EdgeSMAP = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors — ' + '<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>',
+let EdgeSMAP = L.tileLayer('https://edge-tiles.sadata.id/{z}/{x}/{y}.png', {
   maxZoom: 20
 })
 
